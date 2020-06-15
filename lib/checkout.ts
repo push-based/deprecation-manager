24e0267ae1b0b67ee292ba8753855c0236d9bd3e
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2020-06-15 20:09:53 +0200

    fix: checkout

--- lib/checkout.ts
+++ lib/checkout.ts
@@ -2,29 +2,9 @@ import * as cp from "child_process";
 import { CrawlConfig } from "./models";
 
 export async function checkout(config: CrawlConfig) {
-  const tagsString = await git([
-    `log --tags --simplify-by-decoration --pretty="%d;%ci"`,
-  ]);
-
-  const tagWithDate = tagsString
-    .split("\r\n")
-    .filter(Boolean)
-    .slice(0, 3)
-    .concat(` (tag: master);${new Date().toISOString()}`)
-    .map((t) => {
-      const tag = t.substring(7, t.indexOf(")"));
-      const date = t.split(";")[1];
-      return [tag, date.substr(0, 10)];
-    })
-    .find(([tag]) => tag === config.gitTag);
-
-  if (!tagWithDate) {
-    throw new Error(`Tag "${config.gitTag}" not found`);
-  }
-
   await git([`checkout`, config.gitTag]);
-
-  return tagWithDate[1];
+  const date = await git([`log -1 --format=%ai ${config.gitTag}`]);
+  return date;
 }
 
 function git(args: string[]): Promise<string> {
