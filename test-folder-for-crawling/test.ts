557063a5b32cf9e35052232e7e162791da66ae1b
Michael Hladky <michael@hladky.at>
2020-07-28 20:05:15 +0200

    added scripts and folder for testing the crawler in development

---
+++ test-folder-for-crawling/test.ts
@@ -0,0 +1,7 @@
+import { deprecate } from "util";
+
+// @deprecated test Details: {@link https://rxjs.dev/deprecations#undefined}
+const t = 0;
+
+// @deprecated Test Details: {@link https://rxjs.dev/deprecations#undefined}
+const t1 = 0;
