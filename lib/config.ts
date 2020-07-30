9bd712d4677ba43a824dc9824c7aec9ff8374603
Michael Hladky <michael@hladky.at>
2020-07-30 19:24:37 +0200

    tag suggestion (#21)

--- lib/config.ts
+++ lib/config.ts
@@ -4,6 +4,7 @@ import { glob } from "glob";
 import { CrawlConfig } from "./models";
 import { REPO_CONFIG_PATH } from "./constants";
 import { readFile, updateRepoConfig } from "./utils";
+import { execSync } from "child_process";
 
 export async function getConfig(): Promise<CrawlConfig> {
   const repoConfigFile = readFile(REPO_CONFIG_PATH) || "{}";
@@ -13,21 +14,25 @@ export async function getConfig(): Promise<CrawlConfig> {
   if (tsConfigFiles.length === 0) {
     throw Error("We need a tsconfig file to crawl");
   }
-
+  const defaultTag = "master";
+  const tagChoices = [...getGitHubBranches(defaultTag), ...getGitHubTags()];
   const userConfig: CrawlConfig = await prompt([
     {
-      type: "input",
+      type: "select",
       name: "gitTag",
       message: `What git tag do you want to crawl?`,
-      initial: process.argv.slice(2)[0] || "master",
       skip: !!process.argv.slice(2)[0],
+      // @NOTICE: by using choices here the initial value has to be typed as number.
+      // However, passing a string works :)
+      initial: process.argv.slice(2)[0] as unknown as number || defaultTag as unknown as number,
+      choices: tagChoices
     },
     {
       type: "input",
       name: "outputDirectory",
       message: "What's the output directory?",
       initial: repoConfig.outputDirectory || "./deprecations",
-      skip: !!repoConfig.outputDirectory,
+      skip: !!repoConfig.outputDirectory
     },
     {
       type: "select",
@@ -41,14 +46,14 @@ export async function getConfig(): Promise<CrawlConfig> {
         repoConfig.tsConfigPath ||
         tsConfigFiles.find((p) => p === "tsconfig.json") ||
         tsConfigFiles[0],
-      skip: !!repoConfig.tsConfigPath || tsConfigFiles.length === 1,
+      skip: !!repoConfig.tsConfigPath || tsConfigFiles.length === 1
     },
     {
       type: "input",
       name: "deprecationComment",
       message: "What's the deprecation keyword to look for?",
       initial: repoConfig.deprecationComment || "@deprecated",
-      skip: !!repoConfig.deprecationComment,
+      skip: !!repoConfig.deprecationComment
     },
     {
       type: "input",
@@ -56,8 +61,8 @@ export async function getConfig(): Promise<CrawlConfig> {
       message:
         "What's the deprecation link to the docs (the deprecation uuid will be appended to this)?",
       initial: repoConfig.deprecationLink || "https://rxjs.dev/deprecations",
-      skip: !!repoConfig.deprecationLink,
-    },
+      skip: !!repoConfig.deprecationLink
+    }
   ]);
 
   const config = { groups: [], ...repoConfig, ...userConfig };
@@ -68,4 +73,18 @@ export async function getConfig(): Promise<CrawlConfig> {
 export function findTsConfigFiles() {
   const tsConfigs = glob.sync("**/*tsconfig*.json", { ignore: "**/node_modules/**" });
   return tsConfigs;
+}
+
+export function getGitHubTags(): string[] {
+  return execSync("git tag").toString().trim().split("\n").reverse();
+}
+
+export function getGitHubBranches(defaultTag: string): string[] {
+  return [
+    defaultTag,
+    ...execSync("git branch").toString().trim().split("\n")
+      // @TODO remove ugly hack for the `*` char of the current branch
+      .map(i => i.split("* ").join(""))
+      .filter(v => v !== defaultTag)
+  ].reverse();
 }
