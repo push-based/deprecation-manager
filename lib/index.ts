c52714e28b3192785fb543779fdfb425682cec5e
Michael Hladky <michael@hladky.at>
2020-07-31 15:14:51 +0200

    fix(processes): organized functions

--- lib/index.ts
+++ lib/index.ts
@@ -1,13 +1,13 @@
 import { getConfig } from "./config";
 
-import { CrawledRelease, CrawlerProcess } from "./models";
+import { CrawledRelease } from "./models";
 import { crawlDeprecations } from "./crawler";
 import { checkout } from "./checkout";
 
 import { addGrouping } from "./processors/grouping";
 import { addUniqueKey } from "./processors/unique";
 import { addCommentToRepository, generateMarkdown, generateRawJson } from "./output-formatters/";
-import { prompt } from "enquirer";
+import { askToSkip, concat, tap } from "./utils";
 
 (async () => {
   const config = await getConfig();
@@ -57,40 +57,3 @@ import { prompt } from "enquirer";
   concat(processors)(crawledRelease);
 
 })();
-
-function concat<I>(processes: CrawlerProcess<I, I>[]): CrawlerProcess<I, I> {
-  return async function(d: I): Promise<I> {
-    return await processes.reduce(
-      async (deps, processor) => await processor(await deps),
-      Promise.resolve(d)
-    );
-  };
-}
-
-function tap<I>(process: CrawlerProcess<I, I | void>): CrawlerProcess<I, I> {
-  return async function(d: I): Promise<I> {
-    await process(d);
-    return Promise.resolve(d);
-  };
-}
-
-function askToSkip<I>(question: string, process: CrawlerProcess<I, I>): CrawlerProcess<I, I> {
-  return async function(d: I): Promise<I> {
-
-    const answer: { skip: string } = await prompt([{
-      type: "select",
-      name: "skip",
-      message: question,
-      choices: [
-        { name: "Y" },
-        { name: "n", value: false }
-      ]
-    } as any]);
-
-    console.log("answer.skip", answer.skip);
-    if (answer.skip == "n") {
-      return Promise.resolve(d);
-    }
-    return await process(d);
-  };
-}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,8 @@
 import { existsSync, mkdirSync, writeFileSync, readFileSync } from "fs";
-import { CrawlConfig } from "./models";
+import { CrawlConfig, CrawlerProcess } from "./models";
 import { format, resolveConfig } from "prettier";
 import { REPO_CONFIG_PATH } from "./constants";
+import { prompt } from "enquirer";
 
 export function hash(str: string) {
   let s = str.replace(/ /g, "");
@@ -41,111 +42,39 @@ export function readFile(path: string) {
   return "";
 }
 
-// import {
-//   CrawledDeprecation,
-//   CrawledRelease,
-//   NodeTypes,
-//   RawDeprecation,
-//   RawMigrationReleaseItem,
-//   SubjectActionSymbol,
-//   SubjectSymbols,
-// } from "./models";
+export function concat<I>(processes: CrawlerProcess<I, I>[]): CrawlerProcess<I, I> {
+  return async function(d: I): Promise<I> {
+    return await processes.reduce(
+      async (deps, processor) => await processor(await deps),
+      Promise.resolve(d)
+    );
+  };
+}
 
-// export function fillRelease(
-//   r: CrawledRelease = {} as CrawledRelease,
-//   defaultOverrides: Partial<RawMigrationReleaseItem> = {}
-// ): RawMigrationReleaseItem {
-//   const parsedRelease: RawMigrationReleaseItem = {
-//     version: "@TODO",
-//     deprecations: [],
-//     date: "@TODO",
-//     sourceLink: "@TODO",
-//     ...defaultOverrides,
-//   };
-//   (r.date && (parsedRelease.date = r.date)) ||
-//     (parsedRelease.date = new Date("2021-01-01").toISOString());
-//   r.version && (parsedRelease.version = r.version);
-//   parsedRelease.deprecations || (parsedRelease.deprecations = []);
-//   return parsedRelease;
-// }
+export function tap<I>(process: CrawlerProcess<I, I | void>): CrawlerProcess<I, I> {
+  return async function(d: I): Promise<I> {
+    await process(d);
+    return Promise.resolve(d);
+  };
+}
 
-// export function fillDeprecation(
-//   d: CrawledDeprecation = {} as CrawledDeprecation,
-//   defaultOverrides: Partial<RawDeprecation> = {}
-// ): RawDeprecation {
-//   const parsedDeprecation: RawDeprecation = {
-//     itemType: "deprecation",
-//     subject: "~subject~",
-//     subjectAction: "~subjectActionSymbol~-~subjectAction~",
-//     subjectSymbol: "~subjectSymbol~" as any,
-//     reason: "@TODO",
-//     implication: "@TODO",
-//     sourceLink: "@TODO",
-//     deprecationMsgCode: "@TODO",
-//     breakingChangeVersion: "@TODO",
-//     breakingChangeSubjectAction: "@TODO",
-//     breakingChangeMsg: "@TODO",
-//     exampleAfter: "@TODO",
-//     exampleBefore: "@TODO",
-//     ...defaultOverrides,
-//   };
+export function askToSkip<I>(question: string, process: CrawlerProcess<I, I>): CrawlerProcess<I, I> {
+  return async function(d: I): Promise<I> {
 
-//   const subjectSymbol = getSubjectSymbolFormTSType(d.type);
-//   const subjectActionSymbol = getSubjectActionSymbolFormTSType(d.type);
-//   const subSubject = d.name.split(".")[1] ? d.name.split(".")[1] : "";
-//   const subjectAction = "~subjectAction~";
+    const answer: { skip: string } = await prompt([{
+      type: "select",
+      name: "skip",
+      message: question,
+      choices: [
+        { name: "Y" },
+        { name: "n", value: false }
+      ]
+    } as any]);
 
-//   parsedDeprecation.subject = d.name.split(".")[0];
-//   parsedDeprecation.subjectSymbol = subjectSymbol;
-//   parsedDeprecation.subjectAction = [
-//     subjectActionSymbol,
-//     subSubject,
-//     subjectAction,
-//   ]
-//     .filter((v) => !!v)
-//     .map(trimReservedUrlChars)
-//     .map(trimReservedLinkChars)
-//     .join("-");
-//   parsedDeprecation.sourceLink = d.sourceLink;
-//   parsedDeprecation.deprecationMsgCode = d.deprecationMsg;
-//   return parsedDeprecation;
-// }
-
-// function getSubjectSymbolFormTSType(type: string): string {
-//   switch (type) {
-//     case NodeTypes.ClassDeclaration:
-//     case NodeTypes.MethodDefinition:
-//       return SubjectSymbols.class;
-//     default:
-//       return "~subjectSymbol~";
-//   }
-// }
-
-// function trimReservedUrlChars(str: string): string {
-//   // https://www.ietf.org/rfc/rfc2396.txt
-//   const unreservedChars =
-//     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-//     "0123456789" +
-//     "-_.!~*'()".split("");
-//   return str
-//     .split("")
-//     .filter((i) => unreservedChars.includes(i))
-//     .join("");
-// }
-
-// function trimReservedLinkChars(str: string): string {
-//   const reservedChars = "_".split("");
-//   return str
-//     .split("")
-//     .filter((i) => !reservedChars.includes(i))
-//     .join("");
-// }
-
-// function getSubjectActionSymbolFormTSType(type: string): string {
-//   switch (type) {
-//     case NodeTypes.MethodDefinition:
-//       return SubjectActionSymbol.method;
-//     default:
-//       return "~subjectActionSymbol~";
-//   }
-// }
+    console.log("answer.skip", answer.skip);
+    if (answer.skip == "n") {
+      return Promise.resolve(d);
+    }
+    return await process(d);
+  };
+}
