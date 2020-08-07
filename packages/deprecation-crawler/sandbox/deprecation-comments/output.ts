fdeec2280e54a922d4641915fc17fff721219ff7
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2020-08-07 18:36:02 +0200

    fix: make ruid OS independant

--- packages/deprecation-crawler/sandbox/deprecation-comments/output.ts
+++ packages/deprecation-crawler/sandbox/deprecation-comments/output.ts
@@ -2,14 +2,14 @@
  * Examples in this file should multiple commenting styles.
  */
 
-/** @deprecated comment-style single line deprecation Details: {@link sandbox-deprecation-link#516907057} */
+/** @deprecated comment-style single line deprecation Details: {@link sandbox-deprecation-link#3397214801} */
 function foo() {
   return 'foo';
 }
 
 /**
  * @nocollapse
- * @deprecated comment-style deprecation with leading text Details: {@link sandbox-deprecation-link#11092849}
+ * @deprecated comment-style deprecation with leading text Details: {@link sandbox-deprecation-link#456802929}
  */
 function foo2() {
   return 'foo2';
@@ -18,14 +18,14 @@ function foo2() {
 /**
  * This is foo3
  * @method foo3
- * @deprecated comment-style deprecation with leading and trailing text Details: {@link sandbox-deprecation-link#2056677937}
+ * @deprecated comment-style deprecation with leading and trailing text Details: {@link sandbox-deprecation-link#242198417}
  * @return {void}
  */
 function foo3() {
   return 'foo3';
 }
 
-/** @deprecated comment-style single line deprecation with no space at the end Details: {@link sandbox-deprecation-link#4172387057}*/
+/** @deprecated comment-style single line deprecation with no space at the end Details: {@link sandbox-deprecation-link#2601492017}*/
 function foo4() {
   return 'foo4';
 }
