e185c6a138a93a521141b9ecf1abf0249d8b64fd
Michael Hladky <michael@hladky.at>
2020-08-09 10:38:50 +0200

    feat: sort groups alphabetically and prit tags (#45)
    [skip release]

--- packages/deprecation-crawler/src/lib/config.ts
+++ packages/deprecation-crawler/src/lib/config.ts
@@ -1,116 +1,178 @@
-import { normalize } from "path";
-import { prompt } from "enquirer";
-import { glob } from "glob";
-import { CrawlConfig } from "./models";
-import { CRAWLER_CONFIG_PATH, TSCONFIG_PATH } from "./constants";
-import { readFile, updateRepoConfig } from "./utils";
-import { execSync } from "child_process";
-import * as yargs from "yargs";
+import { normalize } from 'path';
+import { prompt } from 'enquirer';
+import { glob } from 'glob';
+import { CrawlConfig } from './models';
+import {
+  CRAWLER_CONFIG_PATH,
+  TSCONFIG_PATH,
+  DEPRECATIONS_OUTPUT_DIRECTORY,
+} from './constants';
+import { readFile, updateRepoConfig } from './utils';
+import { execSync } from 'child_process';
+import * as yargs from 'yargs';
 
 export async function getConfig(): Promise<CrawlConfig> {
   // Check for path params from cli command
-  const argPath = (yargs.argv.path ? yargs.argv.path : yargs.argv.p ? yargs.argv.p : "").toString().trim();
+  const argPath = (yargs.argv.path
+    ? yargs.argv.path
+    : yargs.argv.p
+    ? yargs.argv.p
+    : ''
+  )
+    .toString()
+    .trim();
   // if no param is given it is '' if param with no value is given it is true
-  const argPathGiven = argPath !== "true" && argPath !== "";
+  const argPathGiven = argPath !== 'true' && argPath !== '';
   const crawlerConfigPath = argPathGiven ? argPath : CRAWLER_CONFIG_PATH;
 
-  const repoConfigFile = readFile(crawlerConfigPath) || "{}";
+  const repoConfigFile = readFile(crawlerConfigPath) || '{}';
   const repoConfig = JSON.parse(repoConfigFile);
 
   const tsConfigFiles = findTsConfigFiles();
   if (tsConfigFiles.length === 0) {
-    throw Error("We need a tsconfig file to crawl");
+    throw Error('We need a tsconfig file to crawl');
   }
 
   // Check for tag params from cli command
-  const argTag = (yargs.argv.tag ? yargs.argv.tag : yargs.argv.t ? yargs.argv.t : "").toString().trim();
+  const argTag = (yargs.argv.tag
+    ? yargs.argv.tag
+    : yargs.argv.t
+    ? yargs.argv.t
+    : ''
+  )
+    .toString()
+    .trim();
   // if no param is given it is '' if param with no value is given it is true
-  const argTagGiven = argTag !== "true" && argTag !== "";
-  const defaultTag = argTagGiven ? argTag : "master";
-  const tagChoices = [...getGitHubBranches(defaultTag), ...getGitHubTags()];
+  const argTagGiven = argTag !== 'true' && argTag !== '';
+  const defaultTag = 'main';
+  const tagChoices = sortTags(
+    getGitHubBranches(defaultTag),
+    getGitHubTags(),
+    defaultTag
+  );
+
   const userConfig: CrawlConfig = await prompt([
     {
-      type: "select",
-      name: "gitTag",
+      type: 'select',
+      name: 'gitTag',
       message: `What git tag do you want to crawl?`,
       skip: argTagGiven as any,
       // @NOTICE: by using choices here the initial value has to be typed as number.
       // However, passing a string works :)
-      initial: ((defaultTag as unknown) as number),
-      choices: tagChoices
+      initial: (defaultTag as unknown) as number,
+      choices: tagChoices,
     },
     {
-      type: "input",
-      name: "outputDirectory",
+      type: 'input',
+      name: 'outputDirectory',
       message: "What's the output directory?",
-      initial: repoConfig.outputDirectory || "./deprecations",
-      skip: !!repoConfig.outputDirectory
+      initial: repoConfig.outputDirectory || DEPRECATIONS_OUTPUT_DIRECTORY,
+      skip: !!repoConfig.outputDirectory,
     },
     {
-      type: "select",
-      name: "tsConfigPath",
+      type: 'select',
+      name: 'tsConfigPath',
       message: "What's the location of the ts config file?",
       choices: findTsConfigFiles(),
       format(value) {
-        return value ? normalize(value) : "";
+        return value ? normalize(value) : '';
       },
       initial:
         repoConfig.tsConfigPath ||
         tsConfigFiles.find((p) => p === TSCONFIG_PATH) ||
         tsConfigFiles[0],
-      skip: !!repoConfig.tsConfigPath || tsConfigFiles.length === 1
+      skip: !!repoConfig.tsConfigPath || tsConfigFiles.length === 1,
     },
     {
-      type: "input",
-      name: "deprecationComment",
+      type: 'input',
+      name: 'deprecationComment',
       message: "What's the deprecation keyword to look for?",
-      initial: repoConfig.deprecationComment || "@deprecated",
-      skip: !!repoConfig.deprecationComment
+      initial: repoConfig.deprecationComment || '@deprecated',
+      skip: !!repoConfig.deprecationComment,
     },
     {
-      type: "input",
-      name: "deprecationLink",
+      type: 'input',
+      name: 'deprecationLink',
       message:
         "What's the deprecation link to the docs (the deprecation ruid will be appended to this)?",
-      initial: repoConfig.deprecationLink || "https://rxjs.dev/deprecations",
-      skip: !!repoConfig.deprecationLink
-    }
+      initial: repoConfig.deprecationLink || 'https://rxjs.dev/deprecations',
+      skip: !!repoConfig.deprecationLink,
+    },
   ]);
 
   const config = {
-    outputFormatters: ["tagBasedMarkdown", "groupBasedMarkdown"],
+    outputFormatters: ['tagBasedMarkdown', 'groupBasedMarkdown'],
     groups: [],
     configPath: crawlerConfigPath,
     ...repoConfig,
-    ...userConfig
+    ...userConfig,
   };
   updateRepoConfig(config);
   return config;
 }
 
 export function findTsConfigFiles() {
-  const tsConfigs = glob.sync("**/*tsconfig*.json", {
-    ignore: "**/node_modules/**"
+  const tsConfigs = glob.sync('**/*tsconfig*.json', {
+    ignore: '**/node_modules/**',
   });
   return [
     TSCONFIG_PATH,
-    ...tsConfigs.filter((i) => i.indexOf(TSCONFIG_PATH) === -1)
+    ...tsConfigs.filter((i) => i.indexOf(TSCONFIG_PATH) === -1),
   ];
 }
 
+function sortTags(tags: string[], branches: string[], first: string): string[] {
+  const branchesWithoutFirst = branches.filter((b) => b !== first);
+  const tagsWithoutFirst = tags.filter((b) => b !== first);
+  // prioritize tags before branches
+  // normalize v1.0.0 and v1.0.0
+  return [
+    ...branchesWithoutFirst.sort(innerSort),
+    ...tagsWithoutFirst.sort(innerSort),
+    first,
+  ];
+
+  function innerSort(a: string, b: string): number {
+    const normalizedA = normalizeSemverIfPresent(a);
+    const normalizedB = normalizeSemverIfPresent(b);
+
+    return (
+      ((/[A-Za-z]/.test(normalizedA) as unknown) as number) -
+        ((/[A-Za-z]/.test(normalizedB) as unknown) as number) ||
+      (normalizedA.toUpperCase() < normalizedB.toUpperCase()
+        ? 1
+        : normalizedA.toUpperCase() > normalizedB.toUpperCase()
+        ? -1
+        : 0)
+    );
+  }
+
+  function normalizeSemverIfPresent(str: string): string {
+    const regex = /^([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$/;
+    const potentialVersionNumber =
+      str[0].toLowerCase() === 'v' ? str.slice(1) : str;
+    return regex.test(potentialVersionNumber) ? potentialVersionNumber : str;
+  }
+}
+
 export function getGitHubTags(): string[] {
-  return execSync("git tag").toString().trim().split("\n").reverse();
+  return execSync('git tag')
+    .toString()
+    .trim()
+    .split('\n')
+    .map((s) => s.trim())
+    .sort();
 }
 
 export function getGitHubBranches(defaultTag: string): string[] {
   return [
     defaultTag,
-    ...execSync("git branch")
+    ...execSync('git branch')
       .toString()
       .trim()
-      .split("\n")
+      .split('\n')
       // @TODO remove ugly hack for the `*` char of the current branch
-      .map((i) => i.split("* ").join(""))
-      .filter((v) => v !== defaultTag)
-  ].reverse();
+      .map((i) => i.split('* ').join(''))
+      .filter((v) => v !== defaultTag),
+  ].sort();
 }
--- packages/deprecation-crawler/src/lib/constants.ts
+++ packages/deprecation-crawler/src/lib/constants.ts
@@ -1,3 +1,4 @@
+export const DEPRECATIONS_OUTPUT_DIRECTORY = 'deprecations';
 export const CRAWLER_CONFIG_PATH = 'deprecation-crawler.config.json';
 export const RAW_DEPRECATION_PATH = 'raw-deprecations.json';
 export const TSCONFIG_PATH = 'tsconfig.deprecation-crawler.json';
