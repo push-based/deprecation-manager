650df2c21ef5b2c12d842a77ff7c995e115347c5
Michael Hladky <michael@hladky.at>
2020-08-11 03:14:15 +0200

    fix(setup): checkout only if branch is different from current one

--- packages/deprecation-crawler/src/lib/checkout.ts
+++ packages/deprecation-crawler/src/lib/checkout.ts
@@ -1,11 +1,17 @@
 import { CrawlConfig } from './models';
 import { git, sandBoxMode } from './utils';
 
-export async function checkout(config: CrawlConfig) {
+export async function checkout(config: CrawlConfig): Promise<string> {
   if (sandBoxMode()) {
     return 'sandbox-date';
   }
-  await git([`checkout`, config.gitTag]);
+
+  const currentBranch = await git([`branch --show-current`]).then((v) =>
+    v.trim()
+  );
+  if (currentBranch !== config.gitTag) {
+    await git([`checkout`, config.gitTag]);
+  }
   const date = await git([`log -1 --format=%ai ${config.gitTag}`]);
   return date.replace('\r\n', '').replace('\n', '');
 }
