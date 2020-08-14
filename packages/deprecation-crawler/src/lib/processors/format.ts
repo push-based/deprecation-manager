9abd3a95c9134fc366554931dba913ec1a6459d8
Michael Hladky <michael@hladky.at>
2020-08-14 03:47:33 +0200

    chore fix lint

--- packages/deprecation-crawler/src/lib/processors/format.ts
+++ packages/deprecation-crawler/src/lib/processors/format.ts
@@ -1,12 +1,12 @@
-import { concat, tap } from "../utils";
-import { CrawledRelease, CrawlerProcess } from "../models";
-import { ensureFormatter } from "../tasks/ensure-fotmatters";
+import { concat, tap } from '../utils';
+import { CrawledRelease, CrawlerProcess } from '../models';
+import { ensureFormatter } from '../tasks/ensure-fotmatters';
 
 // Formatting Job
 export function format(config): CrawlerProcess<CrawledRelease, CrawledRelease> {
   return concat(
-    ensureFormatter(config)
-      .map(([formatterKey, formatter]) =>
-        tap((r: CrawledRelease) => formatter(config, r.deprecations)))
-  )
+    ensureFormatter(config).map(([_, formatter]) =>
+      tap((r: CrawledRelease) => formatter(config, r.deprecations))
+    )
+  );
 }
