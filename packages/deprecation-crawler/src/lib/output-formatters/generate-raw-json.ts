5323b4ad53f07b50aa0f5a0a61c9f0e53b00fed1
Michael Hladky <michael@hladky.at>
2020-08-15 20:45:41 +0200

    fix config 3

--- packages/deprecation-crawler/src/lib/output-formatters/generate-raw-json.ts
+++ packages/deprecation-crawler/src/lib/output-formatters/generate-raw-json.ts
@@ -41,7 +41,7 @@ export async function generateRawJson(
 
   const json = JSON.stringify(content, null, 4);
   const path = join(config.outputDirectory, `${RAW_DEPRECATION_PATH}`);
-  writeFileSync(path, json);
+  writeFileSync(path, json.trim());
 
   console.log(`📝 Raw JSON data up to date under ${path}`);
 }
