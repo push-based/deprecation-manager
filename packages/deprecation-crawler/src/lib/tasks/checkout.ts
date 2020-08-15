54a474ed459c65cb85242342181100d2e8814e6c
Michael Hladky <michael@hladky.at>
2020-08-16 00:26:46 +0200

    revert to checkout branch for now

--- packages/deprecation-crawler/src/lib/tasks/checkout.ts
+++ packages/deprecation-crawler/src/lib/tasks/checkout.ts
@@ -1,5 +1,11 @@
 import { CrawlConfig, CrawlerProcess, CrawledRelease } from '../models';
-import { git, concat, tap, getRemoteUrl } from '../utils';
+import {
+  git,
+  concat,
+  tap,
+  getRemoteUrl,
+  getCurrentBranchOrTag,
+} from '../utils';
 import { ensureGitTag } from './ensure-git-tag';
 
 /**
@@ -9,7 +15,7 @@ import { ensureGitTag } from './ensure-git-tag';
 export function checkout(config: CrawlConfig): CrawlerProcess {
   return concat([
     ensureGitTag(config),
-    tap(async (r) => await checkoutTag(r)),
+    tap(async (r) => await checkoutBranch(r)),
     async (r): Promise<CrawledRelease> => {
       const date = await getBranchDate(r);
       const remoteUrl = await getRemoteUrl();
@@ -21,9 +27,16 @@ export function checkout(config: CrawlConfig): CrawlerProcess {
     },
   ]);
 }
-
+/*
 async function checkoutTag(r: CrawledRelease): Promise<void> {
   await git([`checkout tags/${r.tag.name}`]);
+}
+ */
+async function checkoutBranch(r: CrawledRelease) {
+  const currentBranchOrTag = await getCurrentBranchOrTag();
+  if (currentBranchOrTag !== r.tag.name) {
+    await git([`checkout`, r.tag.name]);
+  }
 }
 
 async function getBranchDate(r: CrawledRelease) {
