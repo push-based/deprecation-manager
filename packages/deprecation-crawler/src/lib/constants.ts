65afcae529772ac7890e15622903486aa4bd17f6
Michael Hladky <michael@hladky.at>
2020-08-15 20:02:18 +0200

    feat(setup): introduced deprecation tag

--- packages/deprecation-crawler/src/lib/constants.ts
+++ packages/deprecation-crawler/src/lib/constants.ts
@@ -2,6 +2,7 @@ export const DEPRECATIONS_OUTPUT_DIRECTORY = 'deprecations';
 export const CRAWLER_CONFIG_PATH = 'deprecation-crawler.config.json';
 export const RAW_DEPRECATION_PATH = 'raw-deprecations.json';
 export const TSCONFIG_PATH = 'tsconfig.deprecation-crawler.json';
+export const DEFAULT_DEPRECATION_MSG_TOKEN = '@deprecated';
 export const DEFAULT_COMMIT_MESSAGE =
   'docs(deprecation-manager): sync deprecations';
 
