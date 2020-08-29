9e37ffd42f7f00ce7250b83deda5857a3de95ad9
Michael Hladky <michael@hladky.at>
2020-08-29 15:13:09 +0200

    feat(formatters): deprecation index cleanup

--- packages/deprecation-crawler/src/lib/output-formatters/index.ts
+++ packages/deprecation-crawler/src/lib/output-formatters/index.ts
@@ -1,9 +1,12 @@
 import { generateDeprecationIndex } from './markdown/deprecation-index.md.formatter';
 // @TODO remove imports
-export { addCommentToRepository } from './add-comments-to-repository';
-export { generateRawJson } from './generate-raw-json';
 import { generateTagBasedFormatter } from './generate-markdown';
 import { generateGroupBasedFormatter } from './markdown/generate-group-based-formatter';
+
+export { generateTagBasedFormatter } from './generate-markdown';
+export { generateGroupBasedFormatter } from './markdown/generate-group-based-formatter';
+export { addCommentToRepository } from './add-comments-to-repository';
+export { generateRawJson } from './generate-raw-json';
 
 export const builtInFormatter = {
   tagBasedMarkdown: generateTagBasedFormatter,
