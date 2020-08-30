2c4ef619518af7b54da2f7ec444170501e10ee12
Michael Hladky <michael@hladky.at>
2020-08-30 02:22:29 +0200

    renamed functions

--- packages/deprecation-crawler/src/lib/output-formatters/index.ts
+++ packages/deprecation-crawler/src/lib/output-formatters/index.ts
@@ -1,9 +1,9 @@
 export {addCommentToRepository} from './add-comments-to-repository';
 export {generateRawJson} from './generate-raw-json';
-import {generateTagBasedFormatter} from './markdown/tag-based.md-formatter';
-import {generateGroupBasedFormatter} from './markdown/group-based.md.formatter';
+import {generateTagBasedFormat} from './markdown/tag-based.md-formatter';
+import {generateGroupBasedFormat} from './markdown/group-based.md.formatter';
 
 export const builtInFormatter = {
-  tagBasedMarkdown: generateTagBasedFormatter,
-  groupBasedMarkdown: generateGroupBasedFormatter
+  tagBasedMarkdown: generateTagBasedFormat,
+  groupBasedMarkdown: generateGroupBasedFormat
 }
