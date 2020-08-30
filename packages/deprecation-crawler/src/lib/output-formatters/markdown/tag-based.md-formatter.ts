9ed66acd578f2fd9e3f1813d6ee4cf076a7db18f
Michael Hladky <michael@hladky.at>
2020-08-30 02:22:29 +0200

    renamed functions

--- packages/deprecation-crawler/src/lib/output-formatters/markdown/tag-based.md-formatter.ts
+++ packages/deprecation-crawler/src/lib/output-formatters/markdown/tag-based.md-formatter.ts
@@ -4,7 +4,7 @@ import { CrawlConfig, CrawledRelease, Deprecation } from '../../models';
 import { ensureDirExists, toFileName, formatCode } from '../../utils';
 import { EOL } from 'os';
 
-export async function generateTagBasedFormatter(
+export async function generateTagBasedFormat(
   config: CrawlConfig,
   crawledRelease: CrawledRelease
 ): Promise<void> {
