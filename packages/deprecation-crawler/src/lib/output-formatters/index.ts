2956ff2d7935eefef5697323900ccc8dbaf89a12
Michael Hladky <michael@hladky.at>
2020-08-30 02:18:47 +0200

    renamed tag-based formatter file

--- packages/deprecation-crawler/src/lib/output-formatters/index.ts
+++ packages/deprecation-crawler/src/lib/output-formatters/index.ts
@@ -1,6 +1,6 @@
 export {addCommentToRepository} from './add-comments-to-repository';
 export {generateRawJson} from './generate-raw-json';
-import {generateTagBasedFormatter} from './markdown/generate-markdown';
+import {generateTagBasedFormatter} from './markdown/tag-based.md-formatter';
 import {generateGroupBasedFormatter} from './markdown/group-based.md.formatter';
 
 export const builtInFormatter = {
