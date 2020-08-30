2727464a55dfa27e4831465d256ac8adf0e8624c
Michael Hladky <michael@hladky.at>
2020-08-30 22:19:45 +0200

    fixed imports

--- packages/deprecation-crawler/src/lib/cli/index.ts
+++ packages/deprecation-crawler/src/lib/cli/index.ts
@@ -1,7 +1,6 @@
 import * as yargs from 'yargs';
 import { Options } from 'yargs';
 import { YargsCommandObject } from './model';
-import { COMMANDS, OPTIONS } from '../cli-settings';
 
 export const DEFAULT_COMMAND_NAME = 'default-command';
 
@@ -24,10 +23,12 @@ export function runCli(
   },
   defaultCommand: string = DEFAULT_COMMAND_NAME
 ) {
-  const yargs = setupYargs(COMMANDS, OPTIONS);
+  const yargs = setupYargs(cliCfg.commands, cliCfg.options);
   const argv = yargs.argv;
   const isDefaultCommand = argv._[0] === undefined;
   if (isDefaultCommand) {
-    COMMANDS.find((c) => c.command === defaultCommand).module.handler(argv);
+    cliCfg.commands
+      .find((c) => c.command === defaultCommand)
+      .module.handler(argv);
   }
 }
