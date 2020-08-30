a5fa63c5f4fadd9bd1e09adf2e99d01c85dcdb5f
Michael Hladky <michael@hladky.at>
2020-08-30 02:15:26 +0200

    moved tag-based formatter file

--- packages/deprecation-crawler/src/lib/output-formatters/index.ts
+++ packages/deprecation-crawler/src/lib/output-formatters/index.ts
@@ -1,6 +1,6 @@
 export {addCommentToRepository} from './add-comments-to-repository';
 export {generateRawJson} from './generate-raw-json';
-import {generateTagBasedFormatter} from './generate-markdown';
+import {generateTagBasedFormatter} from './markdown/generate-markdown';
 import {generateGroupBasedFormatter} from './markdown/group-based.md.formatter';
 
 export const builtInFormatter = {
