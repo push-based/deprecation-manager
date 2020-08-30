bc6cfa78e0d41719547c5a684b374acb49a610df
Michael Hladky <michael@hladky.at>
2020-08-30 02:11:11 +0200

    renamed group-based formatter file name

--- packages/deprecation-crawler/src/lib/output-formatters/index.ts
+++ packages/deprecation-crawler/src/lib/output-formatters/index.ts
@@ -1,7 +1,7 @@
 export {addCommentToRepository} from './add-comments-to-repository';
 export {generateRawJson} from './generate-raw-json';
 import {generateTagBasedFormatter} from './generate-markdown';
-import {generateGroupBasedFormatter} from './markdown/generate-group-based-formatter';
+import {generateGroupBasedFormatter} from './markdown/group-based.md.formatter';
 
 export const builtInFormatter = {
   tagBasedMarkdown: generateTagBasedFormatter,
