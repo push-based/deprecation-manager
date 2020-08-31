4bd491dec3e55ab2fe1e44ec84917df9f60aa572
Michael Hladky <michael@hladky.at>
2020-08-31 02:12:41 +0200

    fix imports

--- packages/deprecation-crawler/src/lib/output-formatters/index.ts
+++ packages/deprecation-crawler/src/lib/output-formatters/index.ts
@@ -1,11 +1,9 @@
 import { generateDeprecationIndex } from './markdown/deprecation-index.md.formatter';
-import { generateTagBasedFormatter } from './generate-markdown';
-import { generateGroupBasedFormatter } from './markdown/generate-group-based-formatter';
-export { generateTagBasedFormatter } from './generate-markdown';
-export { generateGroupBasedFormatter } from './markdown/generate-group-based-formatter';
+import { generateTagBasedFormat } from './markdown/tag-based.md-formatter';
+import { generateGroupBasedFormat } from './markdown/group-based.md.formatter';
 
 export const builtInFormatter = {
-  tagBasedMarkdown: generateTagBasedFormatter,
-  groupBasedMarkdown: generateGroupBasedFormatter,
+  tagBasedMarkdown: generateTagBasedFormat,
+  groupBasedMarkdown: generateGroupBasedFormat,
   deprecationIndex: generateDeprecationIndex,
 };
