538f80119b785a89d7e62a91c706e2ac3a9a739e
Michael Hladky <michael@hladky.at>
2020-09-26 12:32:16 +0200

    fix(github-action): use interactive flag with false (#120)

---
+++ packages/deprecation-crawler/github-actions-test/src/initial-test.ts
@@ -0,0 +1,8 @@
+/**
+ * Initial GitHub Action Test file
+ */
+
+/**
+ * @deprecated for the initial github action test Details: {@link /deprecations#3634283877}
+ */
+const initialGitHubActionTest = 0;
